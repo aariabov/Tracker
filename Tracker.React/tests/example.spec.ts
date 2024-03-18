cef4112b477800542190254c8eaea0c7e1671fd3
Riabov.AA <riabov.aa@parmalogica.ru>
2024-03-18 12:11:29 +0500

    CR

--- Tracker.React/tests/example.spec.ts
+++ Tracker.React/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('login page', async ({ page }) => {
-  await page.goto('http://localhost:8080/login');
+  await page.goto('/login');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle('Task tracker');
@@ -10,24 +10,24 @@ test('login page', async ({ page }) => {
 });
 
 test('login', async ({ page }) => {
-  await page.goto('http://localhost:8080/login');
+  await page.goto('/login');
   await page.locator("[name='email']").fill('admin@parma.ru');
   await page.locator("[name='password']").fill('1');
   await page.getByRole('button', { name: 'Войти' }).click();
 
-  await page.waitForURL('http://localhost:8080/');
+  await page.waitForURL('/');
   await expect(page.getByRole('menu')).toBeVisible();
 });
 
 test('generated login', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
-  await page.goto('http://localhost:8080/login');
+  await page.goto('/');
+  await page.goto('/login');
   await page.getByLabel('Email').click();
   await page.getByLabel('Email').fill('admin@parma.ru');
   await page.getByLabel('Пароль').click();
   await page.getByLabel('Пароль').fill('1');
   await page.getByRole('button', { name: 'Войти' }).click();
 
-  await page.waitForURL('http://localhost:8080/');
+  await page.waitForURL('/');
   await expect(page.getByRole('menu')).toBeVisible();
 });
